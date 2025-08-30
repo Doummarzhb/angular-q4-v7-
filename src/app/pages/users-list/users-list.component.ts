6d5c3c7854d4e89a715fbc10f21a350eff724c73
Doummar M.alzahabi <98951369+Doummarzhb@users.noreply.github.com>
2025-08-30 15:18:27 +0300

    eedit for design

--- src/app/pages/users-list/users-list.component.ts
+++ src/app/pages/users-list/users-list.component.ts
@@ -1,33 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.scss']
+  styleUrls: ['./users-list.component.scss'] ,
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms ease-in', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
-export class UsersListComponent implements OnInit {
 
+export class UsersListComponent implements OnInit {
   users = [];
-  loading = false;
   page = 1;
   totalPages = 1;
+  loading = false;
   searchId: number;
 
-  constructor(private userService: UserService, private router: Router) { }
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit() {
-    this.loadUsers();
-    // this.loadUsers(2);
+    this.loadUsers(this.page);
   }
 
-  loadUsers(page: number = 1) {
+  loadUsers(page: number) {
     this.loading = true;
     this.userService.getUsers(page).subscribe(res => {
       this.users = res.data;
-      this.totalPages = res.total_pages;
       this.page = res.page;
+      this.totalPages = res.total_pages;
       this.loading = false;
     });
   }
@@ -37,27 +45,35 @@ export class UsersListComponent implements OnInit {
   }
 
   searchUser() {
-    if (this.searchId) {
-      this.loading = true;
-      this.userService.getUserById(this.searchId).subscribe(res => {
+    if (!this.searchId) {
+      this.loadUsers(1);
+      return;
+    }
+    this.loading = true;
+    this.userService.getUserById(this.searchId).subscribe(res => {
+      if (res && res.data) {
         this.users = [res.data];
         this.totalPages = 1;
         this.page = 1;
-        this.loading = false;
-      }, err => {
+      } else {
         this.users = [];
-        this.loading = false;
-      });
-    } else {
-      this.loadUsers();
-    }
+      }
+      this.loading = false;
+    }, err => {
+      this.users = [];
+      this.loading = false;
+    });
   }
 
   nextPage() {
-    if (this.page < this.totalPages) this.loadUsers(this.page + 1);
+    if (this.page < this.totalPages) {
+      this.loadUsers(this.page + 1);
+    }
   }
 
   prevPage() {
-    if (this.page > 1) this.loadUsers(this.page - 1);
+    if (this.page > 1) {
+      this.loadUsers(this.page - 1);
+    }
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -23,14 +23,22 @@ export class UserService {
 
 getUsers(page: number): Observable<any> {
   const url = `https://reqres.in/api/users?page=${page}`;
-  if (this.cache.has(url)) return of(this.cache.get(url));
-  return this.http.get(url).pipe(tap(data => this.cache.set(url, data)));
+  if (this.cache.has(url)) {
+    return of(this.cache.get(url));
+  }
+  return this.http.get(url).pipe(
+    tap(data => this.cache.set(url, data))
+  );
 }
 
 getUserById(id: number): Observable<any> {
   const url = `https://reqres.in/api/users/${id}`;
-  if (this.cache.has(url)) return of(this.cache.get(url));
-  return this.http.get(url).pipe(tap(data => this.cache.set(url, data)));
+  if (this.cache.has(url)) {
+    return of(this.cache.get(url));
+  }
+  return this.http.get(url).pipe(
+    tap(data => this.cache.set(url, data))
+  );
 }
 
 
